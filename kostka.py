0e4b2a557878ed87de6b4b4f99de8fbc36a000bc
pangadev <pgacek0@gmail.com>
2019-09-07 12:52:04 +0200

    Przykład kostki do gry

---
+++ kostka.py
@@ -0,0 +1,23 @@
+from random import randint
+
+rand_num = randint(1, 100)
+
+def zgaduj():
+    try:
+        num = int(input("Zgadnij liczbę: "))
+    except ValueError:
+        print("Podaj poprawną liczbę!")
+        (zgaduj())
+    if num > rand_num:
+        print("Za dużo!")
+        zgaduj()
+    elif num < rand_num:
+        if num < 0:
+            print("Podaj liczbę większą od 0")
+        else:
+            print("Za mało!")
+        zgaduj()
+    else:
+        print("Zgadłeś")
+
+(zgaduj())
